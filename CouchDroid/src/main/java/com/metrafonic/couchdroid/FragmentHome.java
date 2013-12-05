3e4461d75a472061240bd349c36941ec1d86b49d
Mathias Hedberg <hedberg.mathias@gmail.com>
2013-12-05 19:05:51 +0100

    added ability to view response output on homefragment

--- CouchDroid/src/main/java/com/metrafonic/couchdroid/FragmentHome.java
+++ CouchDroid/src/main/java/com/metrafonic/couchdroid/FragmentHome.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -21,7 +22,9 @@ public class FragmentHome extends Fragment {
         Bundle bundle = this.getArguments();
         String response = bundle.getString("movielist", "no bundle");
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        TextView textViewResponse = (TextView) rootView.findViewById(R.id.textView);
 
+        textViewResponse.setText(response);
 
         final String PREFS_NAME = "ServerPrefsFile";
         SharedPreferences settings = this.getActivity().getSharedPreferences(PREFS_NAME, 0);
