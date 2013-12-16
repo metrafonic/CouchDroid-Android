fb358dfcd73b91141e4c81e29543cd9980c7c04f
Mathias Hedberg <hedberg.mathias@gmail.com>
2013-12-16 05:30:19 -0500

    Fixed webaddressnew

--- CouchDroid/src/main/java/com/metrafonic/couchdroid/MainActivity.java
+++ CouchDroid/src/main/java/com/metrafonic/couchdroid/MainActivity.java
@@ -230,7 +230,7 @@ public class MainActivity extends FragmentActivity {
                                 @Override
                                 public void onSuccess(String response) {
                                     settings.edit().putString("responsemanage", response).commit();
-                                    clientnew.get(webaddress + "/quality.list", new AsyncHttpResponseHandler() {
+                                    clientnew.get(webaddressnew + "/quality.list", new AsyncHttpResponseHandler() {
                                         @Override
                                         public void onSuccess(String response) {
                                             settings.edit().putString("responsequality", response).commit();
