2dee2105d30638222e4e7d40c024d624cba11f4f
Mathias Hedberg <hedberg.mathias@gmail.com>
2014-02-02 15:58:37 +0100

    test

--- couchdroid/src/main/java/com/metrafonic/couchdroid/Activity_Home.java
+++ couchdroid/src/main/java/com/metrafonic/couchdroid/Activity_Home.java
@@ -25,7 +25,7 @@ public class Activity_Home extends ActionBarActivity implements ActionBar.TabLis
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which will keep every
      * loaded fragment in memory. If this becomes too memory intensive, it
      * may be best to switch to a
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
@@ -33,7 +33,7 @@ public class Activity_Home extends ActionBarActivity implements ActionBar.TabLis
     SectionsPagerAdapter mSectionsPagerAdapter;
 
     /**
-     * The {@link ViewPager} that will host the section contents.
+     * The {@link android.support.v4.view.ViewPager} that will host the section contents.
      */
     ViewPager mViewPager;
 
@@ -116,7 +116,7 @@ public class Activity_Home extends ActionBarActivity implements ActionBar.TabLis
     }
 
     /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * A {@link android.support.v4.app.FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
